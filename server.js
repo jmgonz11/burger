d669b12be1391b29db9800b72772a59a068f0356
Jade Gonzales <jadegonzales@Jades-MacBook-Pro.local>
2018-05-11 12:09:44 -0500

    adding connections to server.js

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
+var exphbs = require("express-handlebars");
 var app = express();
 
 
 app.use(express.static(process.cwd() + "/public"));
-
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
 app.use(methodOverride("_method"));
-
-
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -22,5 +15,18 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller");
 app.use("/", routes);
 
-var port = 3000 || env.process.port;
+var port = 8080 || env.process.port;
 app.listen(port);
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "null",
+    database: "burger_db"
+});
+
+connection.connect(function(err){
+    if(err)throw err;
+    console.log("Connected as id: " + connection.threadId);
+})
+
